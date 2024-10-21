c4bff04c7edba12790ed6033698688453473fb83
yang-ng <ywu3@brynmawr.edu>
2024-10-22 02:24:30 +0800

    finished A06

--- HelloUnity/Assets/Scripts/GazeController.cs
+++ HelloUnity/Assets/Scripts/GazeController.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GazeController : MonoBehaviour
+public class HeadLookController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform gazeTarget;
+    public Transform headJoint;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (gazeTarget == null || headJoint == null)
+        {
+            return;
+        }
+
+        Vector3 directionToGaze = (gazeTarget.position - headJoint.position).normalized;
+
+        Debug.DrawLine(headJoint.position, gazeTarget.position, Color.blue);
+
+        Vector3 currentLookDirection = headJoint.forward;
+
+        Vector3 rotationAxis = Vector3.Cross(currentLookDirection, directionToGaze);
+
+        float rotationAngle = Vector3.Angle(currentLookDirection, directionToGaze);
+
+        Quaternion computedRotation = Quaternion.AngleAxis(rotationAngle, rotationAxis);
+
+        if (headJoint.parent != null)
+        {
+            headJoint.parent.rotation = computedRotation * headJoint.parent.rotation;
+        }
     }
 }
--- HelloUnity/Assets/Scripts/TwoLinkController.cs
+++ HelloUnity/Assets/Scripts/TwoLinkController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TwoLinkController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Transform endEffector;
+    public Transform middleJoint;
+    public Transform rootJoint;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null || endEffector == null || middleJoint == null || rootJoint == null)
+        {
+            return;
+        }
+
+        float distanceToTarget = Vector3.Distance(rootJoint.position, target.position);
+        Debug.DrawLine(rootJoint.position, target.position, Color.green);
+
+        float length1 = Vector3.Distance(rootJoint.position, middleJoint.position);
+        float length2 = Vector3.Distance(middleJoint.position, endEffector.position);
+
+        if (distanceToTarget > (length1 + length2))
+        {
+            return;
+        }
+
+        Vector3 directionToTarget = (target.position - rootJoint.position).normalized;
+        endEffector.position = target.position;
+
+        Debug.DrawLine(rootJoint.position, endEffector.position, Color.red);
+
+        float angleAtRoot = Mathf.Acos((length1 * length1 + distanceToTarget * distanceToTarget - length2 * length2) / (2 * length1 * distanceToTarget)) * Mathf.Rad2Deg;
+        Vector3 bendAxis = Vector3.Cross(rootJoint.forward, directionToTarget).normalized;
+        Quaternion rotationAtRoot = Quaternion.AngleAxis(angleAtRoot, bendAxis);
+
+        rootJoint.rotation = rotationAtRoot * rootJoint.rotation;
+        middleJoint.position = rootJoint.position + (directionToTarget * length1);
+
+        Debug.DrawLine(middleJoint.position, endEffector.position, Color.blue);
     }
 }
